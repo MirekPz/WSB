46a02cd8e69a83593377c4a6bcbd1c311ba08f32
Mirek-WSB <miroslaw.horbaczewski@wsb.poznan.pl>
2020-02-28 16:28:44 +0100

    Kompresja do Zip

--- xml2zip.py
+++ xml2zip.py
@@ -1,14 +1,43 @@
+# Kompresowanie pliku XML do ZIP
+
 import os
 import glob
 import zipfile
+import sys
+
+print("\nKompresowanie pliku XML do ZIP.")
+
+print()
+print("Ścieżka do bieżącej lokalizacji:\t", os.getcwd())
+print()
+print("Zawartość bieżącego katalogu:\t", os.listdir())
+print()
+
+xml_files = []
+files_list = os.listdir()
+for element in files_list:
+    if ".xml" in element:
+        print(element)
+        xml_files.append(element)
+        print(xml_files)
+
+liczba_plikow_XML_w_katalogu = len(xml_files)
+print('Liczba plików XML w bieżącym katalogu:', liczba_plikow_XML_w_katalogu)
+
+if liczba_plikow_XML_w_katalogu == 0:
+    print("Brak plików XML do kompresji!")
+    sys.exit()
 
 katalog = '*.xml'
 # katalog = '/home/mirek/test/*.xml'
 
-
-print("\n")
-
 for file_xml in glob.glob(katalog):
+    print(file_xml)
+    new_file = file_xml[0:-4]
+    print(new_file)
+    os.rename(file_xml, new_file)
+    file_xml = new_file
+    print(file_xml)
     file_zip = zipfile.ZipFile(file_xml + ".zip", "w")
     file_zip.write(file_xml, compress_type=zipfile.ZIP_DEFLATED)
 
