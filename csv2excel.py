26f4f59450cfd2a053a12fc48b60ade025bbd945
Mirek-WSB <miroslaw.horbaczewski@wsb.poznan.pl>
2020-03-09 10:58:11 +0100

    converts CSV file to Excel file

--- csv2excel.py
+++ csv2excel.py
@@ -1,4 +1,22 @@
+# Konwersja plików CSV do formatu Excela 
+
+# uwaga: w folderze "Dane" mpogą być przed konwersją tylko pliki CSV
+
+import os
 import pandas as pd
 
-df = pd.read_csv('plik.csv', sep=';')
-df.to_excel('plik_wynikowy.xlsx', index=None, header=True)
+files_list = os.listdir("Dane")
+print("\nZawartość katalogu przed konwersją plików:\n", files_list)
+
+os.chdir("Dane")
+
+for plik in files_list:
+    df = pd.read_csv(plik, sep=';')
+    df.to_excel(plik[:-4] + '.xlsx', index=None, header=True)
+    # [:-4] - wycina z nazwy pliku tekst: .csv
+
+new_files_list = os.listdir()
+
+print("\nZawartość katalogu po konwersji plików:\n", new_files_list)
+
+os.chdir("..")
